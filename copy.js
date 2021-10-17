6ae9b982d6d3496c5f3a449a88763b9e9aeed0f5
aclist <92275929+aclist@users.noreply.github.com>
2021-10-17 22:40:32 +0900

    more verbose copy tooltip

--- copy.js
+++ copy.js
@@ -1,11 +1,22 @@
-onClick = (event, text) => {
-
+  copy = (clicked_id) => {
   event.preventDefault();
   input = document.createElement("input");
   input.style="position:absolute;opacity:0";
-  input.value = text;
+  input.value = window.location.origin + '/#' + clicked_id;
   document.body.append(input);
   input.select();
   document.execCommand("copy");
   input.remove();
 }
+
+function onClick(clicked_id)
+	{
+		var par = '-§-'
+		var tip = 'Copied!'
+		document.getElementById(clicked_id).innerHTML = tip;
+		copy(clicked_id);
+
+		setTimeout(function(){
+			document.getElementById(clicked_id).innerHTML = par;
+		}, 800);
+	}
